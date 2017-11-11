fb9bf8c6f6389f93cebeb5a4fe06f4f8b52dfee8
Matt McAlister <matt.mcalister93@gmail.com>
2017-11-11 20:44:49 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,4 +22,12 @@ end
 
 def get_english_meaning
   # code goes here
+  lib = load_library
+  result = nil
+  lib.each do |universal, array|
+    if symbol == array[1]
+      result = array[0]
+    end
+  end
+  result
 end
